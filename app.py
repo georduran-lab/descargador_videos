f48887d5bf0d5b1e6747de63359dc680f45bff31
georduran-lab <georgecajamarca62@gmail.com>
2025-09-22 08:15:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -490,4 +490,5 @@ def download_mp3():
 # -------------------------
 if __name__ == "__main__":
     print("📂 Carpeta de descargas usada:", DOWNLOADS_PATH)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
